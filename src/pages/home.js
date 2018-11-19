747773250aa65a99799e51ee410b0e5d0c4bce6e
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-19 20:12:05 +0300

    fix the white line under the section

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import styled from 'styled-components'
+import {connect} from 'react-redux';
 
 import CoverVideo from '../components/UI-components/coverVideo/coverVideos';
 import HeadingTyping from '../components/UI-components/headingTyping/headingTyping';
 
 const home = (props)=>{
+    const Section = styled.section`
+        background-color: ${props.themeColors.colorPrimar};
+    `;
     return (
-        <section>
+        <Section>
             <CoverVideo/>
             <HeadingTyping mainHeading ="Live your " typing={["Dream.","Life!"]}/>
-        </section>
+        </Section>
     );
 }
 
-export default home;
+const mapStateToProps = state=>{
+    return {
+      themeColors: state.themeReducer.themes[state.themeReducer.selectedTheme],
+    }
+  }
+export default connect (mapStateToProps)(home);
