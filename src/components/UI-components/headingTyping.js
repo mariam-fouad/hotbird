8a0de0b5f5b9e39a8e888d484374a0f49624f505
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-19 17:16:53 +0300

    downloas react-typing-animation package

--- src/components/UI-components/headingTyping.js
+++ src/components/UI-components/headingTyping.js
@@ -2,15 +2,34 @@ import React from 'react';
 import {connect} from 'react-redux';
 import styled from 'styled-components'
 
+
 const headingTyping = (props)=>{
+
+    const Heading1 = styled.h1`
+    background-color:#ffffffa3;
+    color:${props.themeColors.colorPrimar};
+    display: inline-block;
+    position: absolute;
+    top: 5rem;
+    left: 7rem;
+    font-size: 2rem;
+    padding: 0.5rem 1rem;
+}
+
+    `;
+
     return (
-        <h1>Live your </h1>
+        <Heading1>
+        {props.mainHeading}
+        <span>{props.typing[0]}</span>
+        </Heading1>
+        
     );
 }
 
 const mapStateToProps = state=>{
     return {
-      themeSelected: state.themeReducer.selectedTheme,
+        themeColors: state.themeReducer.themes[state.themeReducer.selectedTheme],
     }
   }
 export default connect(mapStateToProps)(headingTyping);
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import CoverVideo from '../components/UI-components/coverVideos';
+import HeadingTyping from '../components/UI-components/headingTyping';
 
 const home = (props)=>{
     return (
         <section>
             <CoverVideo/>
+            <HeadingTyping mainHeading ="Live your " typing={["Life.","Dream!"]}/>
         </section>
     );
 }
