b3ef69247b63f175a5079b4617f50039728a3150
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-21 21:56:01 +0300

    add the themes color

--- src/components/UI-components/themePicker/themePicker.js
+++ src/components/UI-components/themePicker/themePicker.js
@@ -3,16 +3,60 @@ import styled from 'styled-components';
 import {connect} from 'react-redux';
 
 const themePicker = (props)=>{
+
+    const TopLeftQ = styled.div`
+        width: 7rem;
+        height: 7rem;
+        background: ${props.themeColors[0].colorPrimary};
+        border-radius: 7rem 0 0 0;
+        display: inline-block;
+        margin: 0.25rem;
+        border: 0.3rem solid ${props.themeColors[0].colorSecondary};
+    `;
+    const TopRightQ = styled.div`
+        width: 7rem;
+        height: 7rem;
+        background: ${props.themeColors[1].colorPrimary};
+        border-radius: 0 7rem 0 0;
+        display: inline-block;
+        margin: 0.25rem;
+        border: 0.3rem solid ${props.themeColors[1].colorSecondary};
+    `;
+    const BottomLeftQ = styled.div`
+        width: 7rem;
+        height: 7rem;
+        background: ${props.themeColors[2].colorPrimary};
+        border-radius: 0 0 0 7rem;
+        display: inline-block;
+        margin: 0.25rem;
+        border: 0.3rem solid ${props.themeColors[2].colorSecondary};
+    `;
+    const BottomRightQ = styled.div`
+        width: 7rem;
+        height: 7rem;
+        background: ${props.themeColors[3].colorPrimary};
+        border-radius: 0 0 7rem 0;
+        display: inline-block;
+        margin: 0.25rem;
+        border: 0.3rem solid ${props.themeColors[3].colorSecondary};
+    `;
     return (
         <div>
-            Theme Picker
+            <div>
+                <TopLeftQ/>
+                <TopRightQ/>
+            </div>
+            <div>
+                <BottomLeftQ/>
+                <BottomRightQ/>
+            </div>
         </div>
     );
 }
 
 const mapStateToProps = state=>{
     return {
-        themeColors: state.themeReducer.themes[state.themeReducer.selectedTheme],
+        themeColors: state.themeReducer.themes,
     }
   }
 export default connect(mapStateToProps)(themePicker);
