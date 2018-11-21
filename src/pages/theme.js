0d985ad39f7c20739d988c8b013bb90b811b93b8
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-21 21:10:14 +0300

    update the h1 color

--- src/pages/theme.js
+++ src/pages/theme.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import {connect} from 'react-redux';
+
 import ThemePicker from '../components/UI-components/themePicker/themePicker';
 
 const theme = (props)=>{
@@ -7,12 +9,20 @@ const theme = (props)=>{
     const Main = styled.main`
         margin-top: 5rem;
     `;
+    const H1 = styled.h1`
+        color: ${props.themeColors.colorPrimar};
+    `;
     return (
         <Main>
-            <h1> Pick your theme </h1>
+            <H1> Pick your Theme </H1>
             <ThemePicker/>
         </Main>
     )
 }
 
-export default theme;
+const mapStateToProps = state=>{
+    return {
+        themeColors: state.themeReducer.themes[state.themeReducer.selectedTheme],
+    }
+  }
+export default connect(mapStateToProps)(theme);
