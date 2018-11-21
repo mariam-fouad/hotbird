ec02b2b9719a3cc5b3d028aea066c4ec8efa567e
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-21 20:42:26 +0300

    add the basic route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom' ;
+import { Switch , Route ,withRouter,Redirect} from 'react-router-dom'
 
 import './App.css';
 
 import Header from './components/layout/header/header';
 import Home from './pages/home';
+import Themes from './pages/themes';
 import Footer from './components/layout/footer/footer';
 
 class App extends Component {
   render() {
+
+    let routeSwitch= (
+      <Switch>
+        <Route  path="/thems" component={Themes}/>
+        <Route exact path="/" component={Home}/>
+        <Redirect to='/'/>
+      </Switch>
+  );
     return (
       <div className="App">
         <Header/>
-        <Home/>
+        {routeSwitch}
         <Footer/>
       </div>
     );
