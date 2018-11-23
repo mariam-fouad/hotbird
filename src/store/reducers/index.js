728215291e4dc05adbee8e9154a9b6a0c552bb2f
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-23 14:22:45 +0300

    create hotelReducer and connect it with the root reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,9 @@
 import themeReducer from './themeReduce';
-
+import hotelReducer from './hotelReducer';
 const reducer= (state={},action)=>{
     return {
         themeReducer:themeReducer(state.themeReducer,action),
+        hotelReducer:hotelReducer(state.hotelReducer,action),
     };
   }
 export default reducer;
