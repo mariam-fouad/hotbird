38a2c59fe1c9bd920e651ccdee5ac06f20812855
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-26 19:23:33 +0300

    change the structure of the card

--- src/components/UI-components/longCard/longCard.js
+++ src/components/UI-components/longCard/longCard.js
@@ -4,7 +4,9 @@ import styled from 'styled-components'
 const LongCard = (props)=>{
     const CardDiv = styled.div`
         border: 1px solid ${props.themeColors.colorSecondary};
-        display: inline-block;
+        display: flex;
+        justify-content: space-between;
+        padding: 0.3rem;
         width: 21rem;
         margin-bottom : 1rem;
         box-shadow: 1px 4px 3px #ccc;
@@ -19,8 +21,13 @@ const LongCard = (props)=>{
 
     return (
         <CardDiv>
-            <CardName>{props.name}</CardName>
-            <CardMain>{props.main}</CardMain>
+            <div>
+                Slide
+            </div>
+            <div>
+                <CardName>{props.name}</CardName>
+                <CardMain>{props.main}</CardMain>
+            </div>
         </CardDiv>
     );
 }
