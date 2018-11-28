a8f87acc8bc71e609fe05f3a7567234f2f124a77
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-28 18:39:06 +0300

    create a wish Reducer for the wishList

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import themeReducer from './themeReduce';
 import hotelReducer from './hotelReducer';
+import wishReducer from './wishReducer';
 const reducer= (state={},action)=>{
     return {
         themeReducer:themeReducer(state.themeReducer,action),
         hotelReducer:hotelReducer(state.hotelReducer,action),
+        wishReducer:wishReducer(state.wishReducer,action),
     };
   }
 export default reducer;
