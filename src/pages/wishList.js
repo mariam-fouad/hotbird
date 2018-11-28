14f30a474f208c94e22ff70344444fa0946b66b3
Mariam Fouad <mariam.fouad94@gmail.com>
2018-11-28 21:07:03 +0300

    show the hotels in the wishlist

--- src/pages/wishList.js
+++ src/pages/wishList.js
@@ -3,8 +3,22 @@ import styled from 'styled-components'
 import {connect} from 'react-redux';
 
 const wishList = (props)=>{
+
+    const Main = styled.main`
+    margin-top: 3rem;
+    margin-bottom: 2rem;
+    display:flex;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+    const wishList = props.wish.map(hotel=>
+        <p>{hotel.hotelName}</p>);
     return (
-        <h1>wishList</h1>
+        <Main>
+            {wishList}
+        </Main>
+        
     )
 }
 
