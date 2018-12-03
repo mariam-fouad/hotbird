ca9e91ce3843b561dc77421cc380fb6770a71a02
Mariam Fouad <mariam.fouad94@gmail.com>
2018-12-03 18:57:06 +0300

    make the backdrop fixed postion

--- src/components/UI-components/Backdrop/Backdrop.js
+++ src/components/UI-components/Backdrop/Backdrop.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const backdrop = (props)=>{
     const Backdrop = styled.div`
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     width: 100%;
