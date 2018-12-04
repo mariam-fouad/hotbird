0ef2e7025a988e160448dcb27f764f3653d3571a
Mariam Fouad <mariam.fouad94@gmail.com>
2018-12-04 22:10:51 +0300

    create a rating Reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,13 @@
 import themeReducer from './themeReduce';
 import hotelReducer from './hotelReducer';
 import wishReducer from './wishReducer';
+import ratingReducer from './ratingReducer';
 const reducer= (state={},action)=>{
     return {
         themeReducer:themeReducer(state.themeReducer,action),
         hotelReducer:hotelReducer(state.hotelReducer,action),
         wishReducer:wishReducer(state.wishReducer,action),
+        ratingReducer: ratingReducer (state.ratingReducer, action),
     };
   }
 export default reducer;
---
+++ src/store/reducers/ratingReducer.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actions/actionTypes';
+const initialState = {
+   rateing:[
+       {
+            id:2,
+            hotelName:"Sun Shine",
+            rate:8,
+            rateMessage:"Enjoy the sea view and the warm sun. Great hoilday I recommend it",
+       },
+       {
+        id:4,
+        hotelName:"Paradise",
+        rate:10,
+        rateMessage:"As it name suggest a ture paradise in earth. Definitely going again",
+   },
+
+   ]
+  };
+
+
+  const reducer = (state=initialState,action)=>{
+    switch (action.type) {
+        default:return state;
+    }
+  }
+
+
+export default reducer;
