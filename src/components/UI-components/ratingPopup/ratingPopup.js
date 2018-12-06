d8f8ba081770ae09a31213a19811d9fdc076707e
Mariam Fouad <mariam.fouad94@gmail.com>
2018-12-06 17:17:08 +0300

    create a ratingpopup

---
+++ src/components/UI-components/ratingPopup/ratingPopup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ratingPopup = (props)=>{
+    return (
+        <p>
+            ratingPopup
+        </p>
+    )
+}
+
+export default ratingPopup;
