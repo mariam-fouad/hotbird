426ebfaeb0f549236a2ae9373a56683cda2458bf
Mariam Fouad <mariam.fouad94@gmail.com>
2018-12-12 23:01:20 +0300

    Updates

---
+++ precache-manifest.3e56a06969002f9811fdd6c8feb5559e.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c2e99d26a8d35cdd4e38daca1b84769",
+    "url": "/static/media/1.0c2e99d2.jpg"
+  },
+  {
+    "revision": "d5d8744799c4f7187bf6",
+    "url": "/static/css/main.4eaedc61.chunk.css"
+  },
+  {
+    "revision": "4ec7d81a84d73af3f01b",
+    "url": "/static/js/1.4ec7d81a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b42d7a8521708db7b6eb6748483b1ac3",
+    "url": "/static/media/logo.b42d7a85.png"
+  },
+  {
+    "revision": "b8bbef3c87808d6c461c41a003285af9",
+    "url": "/static/media/Candolim-Beach.b8bbef3c.mp4"
+  },
+  {
+    "revision": "1ef095cadde9733187ac6847f3c32051",
+    "url": "/static/media/Candolim-Beach.1ef095ca.webm"
+  },
+  {
+    "revision": "81187717b30b8a96f0ce3d5be84d06ef",
+    "url": "/static/media/Two-Swimmers.81187717.mp4"
+  },
+  {
+    "revision": "f90d4a06367032429be17f7819855207",
+    "url": "/static/media/Two-Swimmers.f90d4a06.webm"
+  },
+  {
+    "revision": "f9b1cedd0123c2fe3b71827da77cdf6e",
+    "url": "/static/media/Perfect_Hour.f9b1cedd.mp4"
+  },
+  {
+    "revision": "6b4261d2aaf573942035c2ad395980b3",
+    "url": "/static/media/Perfect_Hour.6b4261d2.webm"
+  },
+  {
+    "revision": "52e9f28c0f607392e44ab660e40273ed",
+    "url": "/static/media/Vacay_Mode.52e9f28c.mp4"
+  },
+  {
+    "revision": "65d27e42c503792cc0e77b4ce603b942",
+    "url": "/static/media/Vacay_Mode.65d27e42.webm"
+  },
+  {
+    "revision": "5c55946ef96b034f428b0ffa07062889",
+    "url": "/static/media/1.5c55946e.jpg"
+  },
+  {
+    "revision": "574c426e0c0cf5959f57d9ca0da95107",
+    "url": "/static/media/1.574c426e.jpg"
+  },
+  {
+    "revision": "0ff25082dd2faf2e70bbbd4e715817a5",
+    "url": "/static/media/1.0ff25082.jpg"
+  },
+  {
+    "revision": "d5d8744799c4f7187bf6",
+    "url": "/static/js/main.d5d87447.chunk.js"
+  },
+  {
+    "revision": "3cc49b7ef74219568e16ba9bf57a739f",
+    "url": "/static/media/1.3cc49b7e.jpg"
+  },
+  {
+    "revision": "3a14873de66c70f59b92fb60eb3efa3b",
+    "url": "/static/media/1.3a14873d.jpg"
+  },
+  {
+    "revision": "6aaee5728d1e9c445cded7a0b8c6d8af",
+    "url": "/static/media/2.6aaee572.jpg"
+  },
+  {
+    "revision": "58793fa8f4bc4f3838702d9812690413",
+    "url": "/static/media/2.58793fa8.jpg"
+  },
+  {
+    "revision": "eefac7f021c619e582a73872d63055ef",
+    "url": "/static/media/2.eefac7f0.jpg"
+  },
+  {
+    "revision": "98fe1d55cb21ebd7c0ea09ce5edece3b",
+    "url": "/static/media/2.98fe1d55.jpg"
+  },
+  {
+    "revision": "7714bb536b87767d605576a45e5dc6da",
+    "url": "/static/media/2.7714bb53.jpg"
+  },
+  {
+    "revision": "f2e29c80f3e0699de71a05f9ab25577a",
+    "url": "/static/media/2.f2e29c80.jpg"
+  },
+  {
+    "revision": "9571b48223aec88ca5b28711fb1d23b5",
+    "url": "/static/media/3.9571b482.jpg"
+  },
+  {
+    "revision": "9aaeb32feeed76f56073b5f30934d033",
+    "url": "/static/media/3.9aaeb32f.jpg"
+  },
+  {
+    "revision": "2966033afd119b52052576b12a5daf0e",
+    "url": "/static/media/3.2966033a.jpg"
+  },
+  {
+    "revision": "c485d969e6514c0c8bd9effecca3e82c",
+    "url": "/static/media/3.c485d969.jpg"
+  },
+  {
+    "revision": "9a956ca8ea4e77768b9ba78bf1375db9",
+    "url": "/static/media/3.9a956ca8.jpg"
+  },
+  {
+    "revision": "cab5602307f31a79afa020dcf2a9a097",
+    "url": "/static/media/3.cab56023.jpg"
+  },
+  {
+    "revision": "d9c9dc03bc633e0acfe8823cbd826e04",
+    "url": "/index.html"
+  }
+];
